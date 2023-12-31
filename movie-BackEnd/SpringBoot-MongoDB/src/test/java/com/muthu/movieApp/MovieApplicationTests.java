fb54d9408b64ad6d475c0823798424bd099d01a4
muthuraja107 <133774084+muthuraja107@users.noreply.github.com>
2023-12-31 21:57:25 +0530

    Add files via upload

---
+++ movie-BackEnd/SpringBoot-MongoDB/src/test/java/com/muthu/movieApp/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muthu.movieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
